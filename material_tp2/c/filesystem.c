03ea668bae6df2c56271207057607de379bdc3a7
Max <max.franke@acad.pucrs.br>
2019-11-15 19:31:20 -0300

    dsadsa
    dsadsa

--- material_tp2/c/filesystem.c
+++ material_tp2/c/filesystem.c
@@ -89,30 +89,6 @@ int main(void)
 	int32_t i;
 	struct dir_entry_s dir_entry;
 
-	/* create filesystem.dat if it doesn't exist */
-	f = fopen("filesystem.dat", "a");
-	fclose(f);
-
-	/* initialize the FAT */
-	for (i = 0; i < FAT_BLOCKS; i++)
-		fat[i] = 0x7ffe;
-	fat[ROOT_BLOCK] = 0x7fff;
-	for (i = ROOT_BLOCK + 1; i < BLOCKS; i++)
-		fat[i] = 0;
-	/* write it to disk */
-	write_fat("filesystem.dat", fat);
-
-	/* initialize an empty data block */
-	for (i = 0; i < BLOCK_SIZE; i++)
-		data_block[i] = 0;
-
-	/* write an empty ROOT directory block */
-	write_block("filesystem.dat", ROOT_BLOCK, data_block);
-
-	/* write the remaining data blocks to disk */
-	for (i = ROOT_BLOCK + 1; i < BLOCKS; i++)
-		write_block("filesystem.dat", i, data_block);
-
 	char entrada[25];
 	char exit[] = "exit";
 	while (1 == 1)
@@ -145,6 +121,37 @@ int main(void)
 			dir_entry.first_block = 1111;
 			dir_entry.size = 222;
 			write_dir_entry(ROOT_BLOCK, 0, &dir_entry);
+		}
+
+		else if (!strcmp(entrada,"load")){
+			f = fopen("filesystem.dat", "a");
+			fclose(f);
+		}
+		else if (!strcmp(entrada, "init"))
+		{
+			/* create filesystem.dat if it doesn't exist */
+			f = fopen("filesystem.dat", "a");
+			fclose(f);
+
+			/* initialize the FAT */
+			for (i = 0; i < FAT_BLOCKS; i++)
+				fat[i] = 0x7ffe;
+			fat[ROOT_BLOCK] = 0x7fff;
+			for (i = ROOT_BLOCK + 1; i < BLOCKS; i++)
+				fat[i] = 0;
+			/* write it to disk */
+			write_fat("filesystem.dat", fat);
+
+			/* initialize an empty data block */
+			for (i = 0; i < BLOCK_SIZE; i++)
+				data_block[i] = 0;
+
+			/* write an empty ROOT directory block */
+			write_block("filesystem.dat", ROOT_BLOCK, data_block);
+
+			/* write the remaining data blocks to disk */
+			for (i = ROOT_BLOCK + 1; i < BLOCKS; i++)
+				write_block("filesystem.dat", i, data_block);
 		}
 	}
 	/* fill three root directory entries and list them */
